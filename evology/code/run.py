ad36154ad718ade518ebe2ebd78c350e5510f76a
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2022-02-07 11:58:17 +0000

    Adding Liquidations output

--- evology/code/run.py
+++ evology/code/run.py
@@ -49,7 +49,7 @@ def run(
 
 
 df, pop, ReturnsNT, ReturnsVI, ReturnsTF = run(
-    100, 0, 1000, wealth_coordinates, tqdm_display=False, reset_wealth=False, 
+    30, 0, 20000, wealth_coordinates, tqdm_display=False, reset_wealth=False, 
     ReinvestmentRate= 1.0, InvestmentHorizon = 252,
 )
 
